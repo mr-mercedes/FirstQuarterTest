16cae43992ef98f62e1da35affebca9fce6d9fbb
Ekyd <Ekyd777@gmail.com>
2022-06-16 22:57:21 +0300

    add final version with method

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.WriteLine("Введите размер массива: ");
 int sizeArray = Convert.ToInt32(Console.ReadLine());
-string?[] array = new string [sizeArray];
+string[] array = new string[sizeArray];
 for (int i = 0; i < array.Length; i++)
 {
-    Console.WriteLine($"Введите {i + 1} элемент массива: ");
+    Console.Write($"Введите {i + 1} элемент массива: ");
     array[i] = Console.ReadLine();
 }
 Console.Write("Заданный массив: ");
@@ -12,17 +12,25 @@ for (int i = 0; i < array.Length; i++)
     Console.Write(array[i] + " ");
 }
 Console.WriteLine();
-string? output = string.Empty;
-string?[] newArray = new string [array.Length];
-for (int i = 0; i < array.Length; i++)
-{
-    output = array[i];
-    if (output?.Length > 3)
-        continue;
-    newArray[i] = output;
-}
+string[] newArray = SortArray(array);
 Console.Write("Итоговый массив: ");
 for (int i = 0; i < newArray.Length; i++)
 {
     Console.Write(newArray[i] + " ");
+}
+
+
+string[] SortArray(string[] array)
+{
+    string output = string.Empty;
+    string[] newArray = new string[array.Length];
+    int maxSize = 3;
+    for (int i = 0; i < array.Length; i++)
+    {
+        output = array[i];
+        if (output.Length > maxSize)
+            continue;
+        newArray[i] = output;
+    }
+    return newArray;
 }
