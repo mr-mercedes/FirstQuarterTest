e41c810413534871de7bbc6907b581515027eec4
Ekyd <Ekyd777@gmail.com>
2022-06-16 22:40:26 +0300

    add correct version without methods

--- Task/Program.cs
+++ Task/Program.cs
@@ -11,18 +11,18 @@ for (int i = 0; i < array.Length; i++)
 {
     Console.Write(array[i] + " ");
 }
-string output = string.Empty;
-string[] newArray = new string [array.Length];
+Console.WriteLine();
+string? output = string.Empty;
+string?[] newArray = new string [array.Length];
 for (int i = 0; i < array.Length; i++)
 {
     output = array[i];
-    if (output.Length > 3)
+    if (output?.Length > 3)
         continue;
     newArray[i] = output;
 }
-System.Console.WriteLine();
+Console.Write("Итоговый массив: ");
 for (int i = 0; i < newArray.Length; i++)
 {
     Console.Write(newArray[i] + " ");
 }
-
