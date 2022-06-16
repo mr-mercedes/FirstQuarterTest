ac2fe5291a98b316bfeb6533cf172444f0806a4f
Ekyd <Ekyd777@gmail.com>
2022-06-16 21:07:23 +0300

    add parsing

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {Console.ReadLine()};
+string output = string.Empty;
+for (int i = 0; i < array.Length; i++)
+{
+    output = array[i];
+}
+int countNumbers = 1;
+for (int i = 0; i < output.Length; i++)
+{
+    if (output[i] == ',')
+        countNumbers++;
+}
+string[] newArray = new string [countNumbers];
+int newArrayIndex = 0;
+for (int i = 0; i < output.Length; i++)
+{
+    string subString = string.Empty;
+    while (output[i] != ',')
+    {
+        if (i >= output.Length - 1)
+            break;
+        subString += output[i];
+        i++;
+        if (subString == "" || subString == " ")
+            continue;
+    }
+    newArray[newArrayIndex] = subString;
+    newArrayIndex++;
+    
+}
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.Write(newArray[i] + " ");
+}
